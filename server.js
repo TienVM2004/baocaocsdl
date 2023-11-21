75c07f1a0e3a9c3dfc8eb777bb6757c43fd01f0d
Vũ Minh Tiến <vuminhtien2004@gmail.com>
2023-11-21 08:16:33 +0700

    hehe

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.get('/login', function(req, res) {
 app.get('/register', function(req, res) {
   res.render('register');
 });
+app.get('/delete', function(req, res) {
+  res.render('delete');
+});
 app.get('/success', function(req, res) {
   res.send('Login successful!');
 });
@@ -52,6 +55,17 @@ app.post('/register', async function(req, res) {
       // Send the message as part of the redirect URL
     insert(client, req.body.username, req.body.password);
     res.redirect('/login');
+  }
+});
+
+app.post('/delete', async function(req, res) {
+  const result = await findByUserName(client, req.body.username);
+  if(result) {
+    deleteUser(client, req.body.username);
+    res.redirect('/login');
+  } else {
+      // Send the message as part of the redirect URL
+    res.redirect('/fail');
   }
 });
 
@@ -104,11 +118,20 @@ async function insert(client, username, password) {
   console.log(`Inserted 1 document into collection with ID ${result.insertedId}`);
 }
 
+async function deleteUser(client, username) {
+  const result = await client.db("test").collection("users")
+  .deleteOne({
+      username: username
+              });
+
+  console.log(`Deleted 1 document into collection with ID ${result.insertedId}`);
+}
+
 /**
  * I honestly don't know what this bitch does
  */
 
-const client = new MongoClient('mongodb+srv://ineedsumsnack0308:ineedsumsnack2004@tienvm2004.lfouc8n.mongodb.net/?retryWrites=true&w=majority');
+const client = new MongoClient('mongodb+srv://ineedsumsnack0308:ineedsumsnack2004@tienvm2004.lfouc8n.mongodb.net/?retryWrites=true&w=majority', {maxPoolSize : 10});
 client.connect().then(() => {
     app.listen(port, () => {
         console.log(`Server is running at http://localhost:${port}`)
